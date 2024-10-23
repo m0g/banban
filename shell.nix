645177849bc521455a2553bc50a54b313f574bf3
Loic Nogues <nogues.loic@gmail.com>
2024-10-23 17:54:46 +0200

    nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+    	nodejs_22 
+	openssl 
+	nodePackages_latest.prisma
+    ];
+
+ shellHook = ''
+      export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+      export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+      export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+      export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+    '';
+}
