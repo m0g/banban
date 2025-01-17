f3233904f43dec83a5fd57eaa7264f886cb50305
Loïc Nogues <nogues.loic@gmail.com>
2025-01-17 15:43:08 +0100

    Move navbar in a separate component and bump to v0.3.0

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,67 +1,13 @@
 <script>
-  import {
-    Avatar,
-    Navbar,
-    NavBrand,
-    NavUl,
-    NavLi,
-    Dropdown,
-    DropdownHeader,
-    DropdownItem,
-    DropdownDivider
-  } from 'flowbite-svelte';
-  import { enhance } from '$app/forms';
   import '../app.pcss';
-  import RecentBoards from '../components/RecentBoards.svelte';
-  import { version, dev } from '$app/environment';
+  import NavBar from '../components/NavBar.svelte';
+  import { dev } from '$app/environment';
 
-  let { data } = $props();
+  let { data, children } = $props();
   let devClass = $derived(dev ? 'border-4 border-red-500' : '');
 </script>
 
 <div class="flex h-screen w-full flex-col {devClass}">
-  <Navbar color="dark">
-    <div class="flex gap-2">
-      <NavBrand href="/" class="flex gap-2">
-        <img src="/favicon.svg" alt="BanBan logo" class="w-8" />
-        <div class="flex flex-col">
-          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            BanBan
-          </span>
-          <span class="text-xs">{version}</span>
-        </div>
-      </NavBrand>
-      {#if data.isLoggedIn && data.user.boards.length > 0}
-        <RecentBoards boards={data.user.boards} />
-      {/if}
-      <NavUl>
-        {#if !data.isLoggedIn}
-          <NavLi href="/signin">Sign in</NavLi>
-          <NavLi href="/signup">Sign up</NavLi>
-        {/if}
-      </NavUl>
-    </div>
-    {#if data.isLoggedIn}
-      {#if data.user.avatar}
-        <Avatar id="user-drop" src={`/images/${data.user.avatar}`} class="cursor-pointer" />
-      {:else}
-        <Avatar id="user-drop" class="cursor-pointer" />
-      {/if}
-      <Dropdown triggeredBy="#user-drop" class="rounded-lg border border-gray-500">
-        <DropdownHeader>
-          <span class="block text-sm">{data.user.name}</span>
-          <span class="block truncate text-sm font-medium">{data.user.email}</span>
-        </DropdownHeader>
-        <DropdownItem href={`/users/${data.user.id}`}>Profile</DropdownItem>
-        <DropdownItem href="/settings">Settings</DropdownItem>
-        <DropdownDivider />
-        <DropdownItem>
-          <form method="post" use:enhance action="/signout">
-            <button type="submit">Sign out</button>
-          </form>
-        </DropdownItem>
-      </Dropdown>
-    {/if}
-  </Navbar>
-  <slot />
+  <NavBar {data} />
+  {@render children?.()}
 </div>
